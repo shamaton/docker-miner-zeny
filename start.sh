cf9fedeb5261a065c86e43ac95e4d902f50a1fda
shamaton <shamaton77@gmail.com>
2018-01-10 00:20:13 +0900

    fix start script

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ if [ -z ${POOL} ]; then
 	exit 1
 fi
 
-if [ -n ${WALLET} ]; then
+if [ -n "${WALLET}" ]; then
 	echo "POOL   -> $POOL"
 	echo "WALLET -> $WALLET"
 	echo "thread -> $THREAD_NUM"
 	/root/cpuminer/minerd  -o $POOL -u $WALLET -t $THREAD_NUM
-elif [ -n ${USERPASS} ]; then
+elif [ -n "${USERPASS}" ]; then
 	echo "POOL     -> $POOL"
 	echo "USERPASS -> $USERPASS"
 	echo "thread   -> $THREAD_NUM"
